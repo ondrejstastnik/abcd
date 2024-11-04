b24125349d2f3e4b183b20bd44a133154e77b4dc
ondrejstastnik <117811130+ondrejstastnik@users.noreply.github.com>
2024-11-04 13:23:41 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$neserazen = [3, 7, 1, 2, 10];
+function najdi_nejmensi($pole) {
+    $nejmensi = $pole[0];
+    for ($i = 0; $i < count($pole); $i++)
+    {
+        $aktualni_prvek = $pole[$i];
+        if ($aktualni_prvek < $nejmensi)
+    }
+}
